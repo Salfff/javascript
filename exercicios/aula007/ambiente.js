f7b3666d1f5470a5513fc2f2ed471c81efd60951
Salfff <147937926+Salfff@users.noreply.github.com>
2025-08-12 15:06:21 -0400

    Folder javascript updated

---
+++ exercicios/aula007/ambiente.js
@@ -0,0 +1,18 @@
+// console.log('tudo bem')
+// console.log('tudo bem')
+// console.log('tudo bem')
+// console.log('tudo bem')
+
+// var cont = 1
+
+// while (cont <= 10) {
+//     console.log(`Passo ${cont}`)
+//     cont++
+// }
+
+ var cont = 1
+
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont <= 10)
