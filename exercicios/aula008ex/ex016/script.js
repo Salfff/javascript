7284937e2e6219308b2b5822fc69ad149a9db216
Salfff <147937926+Salfff@users.noreply.github.com>
2025-08-12 15:46:22 -0400

    javascript folder updated

---
+++ exercicios/aula008ex/ex016/script.js
@@ -0,0 +1,15 @@
+var passoInicial = document.getElementById('inputBegin')
+var passoFinal = document.getElementById('inputEnd')
+var passoQuant = document.getElementById('inputQuantPassos')
+var res = document.getElementById('res')
+
+function contarPassos() {
+    cont = passoInicial.value
+    passoFim = passoFinal.value
+    res.innertext = 'ei'
+    while(cont <= passoFim) {
+        
+        
+    }
+    alert(cont)
+}
